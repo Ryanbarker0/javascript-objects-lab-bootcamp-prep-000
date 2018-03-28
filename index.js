794afa528e054beab2c096e22d1b78c788d91f0c
Ryan Barker <ryangeorgebarker0@gmail.com>
2018-03-28 11:35:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ var recipes = { pizza: 'cheese' }
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, { [key]: value })
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign(object, { [key]: value })
+}
+
